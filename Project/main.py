6de5e22f05926e01b3563e25943544370fe04084
914-Matei-Sonia <sonia.matei1@stud.ubbcluj.ro>
2023-11-22 12:17:38 +0200

    fa

--- Project/main.py
+++ Project/main.py
@@ -1,27 +1,36 @@
 from utils.error import LexicalErr
 from scanner import Scanner
+from utils.fa_reader import FiniteAutomata
 
 
 def main():
-    program_path = "programs/program_err.in"
-    st_path = "out/ST.out"
-    pif_path = "out/PIF.out"
-    file = open(program_path, 'r')
-    scanner = Scanner(file)
 
-    try:
-        program = scanner.scan()
-        print("Lexically correct.")
+    fa_path = "definitions/Fa.in"
 
-        with open(st_path, 'w') as file:
-            file.write(str(program.identifier_table.table) + "\n")
-            file.write(str(program.literal_table.table))
+    fa = FiniteAutomata()
+    fa.read_fa(fa_path)
+    fa.display_console()
+    print(fa.check("1234556"))
 
-        with open(pif_path, 'w') as file:
-            file.write(str(program.pif))
-
-    except LexicalErr as e:
-        print(e)
+    # program_path = "programs/program_err.in"
+    # st_path = "out/ST.out"
+    # pif_path = "out/PIF.out"
+    # file = open(program_path, 'r')
+    # scanner = Scanner(file)
+    #
+    # try:
+    #     program = scanner.scan()
+    #     print("Lexically correct.")
+    #
+    #     with open(st_path, 'w') as file:
+    #         file.write(str(program.identifier_table.table) + "\n")
+    #         file.write(str(program.literal_table.table))
+    #
+    #     with open(pif_path, 'w') as file:
+    #         file.write(str(program.pif))
+    #
+    # except LexicalErr as e:
+    #     print(e)
 
 
 if __name__ == '__main__':
